ed25c7e8db76469064e917819c34359846b8779d
aibvera <alejandro.bueno.vera.97@gmail.com>
2024-07-18 23:29:24 -0500

    Commit inicial

---
+++ funcs.js
@@ -0,0 +1,6 @@
+function sumNumbers() {
+    var num1 = parseInt(document.getElementById('n1').value);
+    var num2 = parseInt(document.getElementById('n2').value);
+    var sum = num1 + num2;
+    document.getElementById('result').textContent = sum;
+}
